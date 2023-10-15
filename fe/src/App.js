33a4d735ec928ea60cb48b349d10528395784fcf
haikalshahab93 <haikal.pusri@gmail.com>
2023-10-15 10:59:09 +0700

    fullstack standart be fe menggunakan cors

---
+++ fe/src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [data, setData] = useState(null);
+
+  const fetchData = async () => {
+    const response = await fetch('http://localhost:3001/api/user');
+    const data = await response.json();
+    setData(data);
+  };
+
+  return (
+    <div className="App">
+      <button className="fetch-button" onClick={fetchData}>Fetch Data</button>
+      {data && <div className="data-container">{JSON.stringify(data)}</div>}
+    </div>
+  );
+}
+
+export default App;
